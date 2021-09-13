5e5b84d1a3dafeda3c9cee181033ed3ac6c30590
gravitaliy <polina_0901@mail.ru>
2021-09-13 19:36:52 +0300

    Authentification

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import VeeValidate from 'vee-validate'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+Vue.use(VeeValidate);
+
 new Vue({
   router,
+  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
